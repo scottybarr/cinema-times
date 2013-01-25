45a6a5e73e7f3bee1530cf5227e52ba37bf07088
Scott Barr <scottbarr@gmail.com>
2013-01-25 22:56:18 +0000

    Added init import statements to models

--- cinema_times/models/__init__.py
+++ cinema_times/models/__init__.py
@@ -1,1 +1,8 @@
-__author__ = 'scott'
+from cinema_times.models.audio_type import AudioType
+from cinema_times.models.cinema import Cinema
+from cinema_times.models.cinema_company import CinemaCompany
+from cinema_times.models.film import Film
+from cinema_times.models.film_reel_logger import FilmReelLogger
+from cinema_times.models.schedule import Schedule
+from cinema_times.models.video_type import VideoType
+from cinema_times.models.website import Website
