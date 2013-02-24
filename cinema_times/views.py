0e5d1e9a25ba13dade9edd47561561a8dd36e06a
Scott Barr <scottbarr@gmail.com>
2013-02-24 13:37:49 +0000

    Remove cineworld import from index page.

--- cinema_times/views.py
+++ cinema_times/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from django.utils import simplejson
-from cinema_times.imports.cineworld_cinemas import CineworldImport
-from cinema_times import config
+
 
 def home(request):
     data = simplejson.dumps({'cinemas': []})
