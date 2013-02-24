d0ee5369898b6eee0f278548a953ace3cba4bb61
Scott Barr <scottbarr@gmail.com>
2013-02-24 14:00:43 +0000

    Refactored project structure for a better OOP approach.

---
+++ cinema_times/imports/cinema_import.py
@@ -0,0 +1,34 @@
+from cinema_times.models import CinemaCompany, Cinema
+import requests
+
+class CinemaImport(object):
+    def __init__(self):
+        self.company_id = None
+        self.company_name = None
+
+    def create_cinema_company(self, company_name, url):
+        try:
+            cinema_company = CinemaCompany(
+                company_name=company_name,
+                company_website=url
+            )
+            cinema_company.save()
+        except Exception:
+            cinema_company = CinemaCompany.objects.get(company_name=company_name)
+        return cinema_company
+
+    def check_cinema_exists(self, company_id, cinema_id):
+        cinema = Cinema.objects.get(company_cinema_id=cinema_id, company_id=company_id)
+        return True if cinema else False
+
+    @staticmethod
+    def try_get_attrib(e, key):
+        try:
+            return e.attrib[key]
+        except Exception:
+            return ""
+
+    @staticmethod
+    def get_url(url):
+        req = requests.get(url)
+        return req.content
--- cinema_times/imports/cineworld_cinemas.py
+++ cinema_times/imports/cineworld_cinemas.py
@@ -1,36 +1,25 @@
-from import_helpers import get_url, try_get_attrib
 from lxml import objectify, html
-from cinema_times.models import Cinema, CinemaCompany
+from cinema_times.models import Cinema
+from cinema_import import CinemaImport
 
 
-class CineworldImport:
+class CineworldImport(CinemaImport):
     def __init__(self, config):
-        self.company_name = "Cineworld"
         self.url = config.CINEWORLD_SYNDICATION_URL + "listings.xml"
-        self.company_id = self.create_cinema_company()
-        xml = get_url(self.url)
+        self.company_id = self.create_cinema_company("Cineworld", "http://www.cineworld.co.uk")
+        xml = self.get_url(self.url)
         self.parse_xml(xml)
 
-    def create_cinema_company(self):
-        try:
-            cinema_company = CinemaCompany(
-                company_name=self.company_name,
-                company_website="http://www.cineworld.co.uk"
-            )
-            cinema_company.save()
-        except Exception:
-            cinema_company = CinemaCompany.objects.get(company_name=self.company_name)
-        return cinema_company
 
     def parse_xml(self, xml):
         root = objectify.fromstring(xml)
         for e in root.iterchildren():
             name = e.attrib['name']
-            phone = try_get_attrib(e, 'phone')
-            address = try_get_attrib(e, 'address')
+            phone = self.try_get_attrib(e, 'phone')
+            address = self.try_get_attrib(e, 'address')
             cinema_id = e.attrib['id']
             url = e.attrib['root'] + e.attrib['url']
-            if not self.check_cinema_exists(cinema_id):
+            if not self.check_cinema_exists(self.company_id, cinema_id):
                 lat, long = self.get_coords(url)
                 c = Cinema(
                     cinema_name=name,
@@ -49,7 +38,3 @@ class CineworldImport:
         contents = html.fromstring(contents)
         coords = contents.xpath("//div[contains(@class,'map')]/@data-coordinates")[0]
         return coords.split(',')
-
-    def check_cinema_exists(self, cinema_id):
-        cinema = Cinema.objects.get(company_cinema_id=cinema_id, company_id=self.company_id)
-        return True if cinema else False
