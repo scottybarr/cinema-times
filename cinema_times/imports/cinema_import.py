0028291b9afb92585ad9c282b78d347ac28c90a9
Scott Barr <scottbarr@gmail.com>
2013-04-18 23:32:55 +0100

    Fixing exception in cinema import.

--- cinema_times/imports/cinema_import.py
+++ cinema_times/imports/cinema_import.py
@@ -1,4 +1,5 @@
 from cinema_times.models import CinemaCompany, Cinema
+from django.core.exceptions import ObjectDoesNotExist
 import requests
 
 class CinemaImport(object):
@@ -7,6 +8,10 @@ class CinemaImport(object):
         self.company_name = None
 
     def create_cinema_company(self, company_name, url):
+        cinema_company = CinemaCompany.objects.get(company_name=company_name)
+        if cinema_company:
+            return cinema_company
+
         try:
             cinema_company = CinemaCompany(
                 company_name=company_name,
@@ -14,12 +19,15 @@ class CinemaImport(object):
             )
             cinema_company.save()
         except Exception:
-            cinema_company = CinemaCompany.objects.get(company_name=company_name)
+            pass
         return cinema_company
 
     def check_cinema_exists(self, company_id, cinema_id):
-        cinema = Cinema.objects.get(company_cinema_id=cinema_id, company_id=company_id)
-        return True if cinema else False
+        try:
+            cinema = Cinema.objects.get(company_cinema_id=cinema_id, company_id=company_id)
+            return True if cinema else False
+        except ObjectDoesNotExist:
+            return False
 
     @staticmethod
     def get_url(url):
