348c57a7afefa1b3e02e21b03cc1c39f207fade5
Scott Barr <scottbarr@gmail.com>
2013-04-20 21:27:55 +0100

    Updating audio type model to follow naming conventions.

--- cinema_times/models/audio_type.py
+++ cinema_times/models/audio_type.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class AudioType(models.Model):
-    AudioType_id = models.AutoField(primary_key=True, unique=True)
+    audio_type_id = models.AutoField(primary_key=True, unique=True)
     audio_desc = models.TextField(null=True)
-    AudioType = models.CharField(max_length=20)
+    audio_type = models.CharField(max_length=20)
 
     class Meta:
         app_label = 'cinema_times'
