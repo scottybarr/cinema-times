7af3ef338a44495169f32b41ec64cd581ac87c0d
Scott Barr <scottbarr@gmail.com>
2013-04-21 20:28:01 +0100

    Use xmltodict to import cinemas.

--- cinema_times/imports/cineworld_cinemas.py
+++ cinema_times/imports/cineworld_cinemas.py
@@ -1,27 +1,28 @@
-from lxml import objectify, html
+from lxml import html
 from cinema_times.models import Cinema
 from cinema_import import CinemaImport
+import xmltodict
 
 
 class CineworldCinemas(CinemaImport):
     def __init__(self, config):
         self.company_id = self.create_cinema_company("Cineworld", "http://www.cineworld.co.uk")
-        xml = self.get_url(config.CINEWORLD_SYNDICATION_URL + "listings.xml")
-        self.parse_xml(xml)
+        self.xml = self.get_url(config.CINEWORLD_SYNDICATION_URL + "cinemas.xml")
 
-    def parse_xml(self, xml):
-        root = objectify.fromstring(xml)
-        for e in root.iterchildren():
+    def parse_cinemas(self):
+        all_cinemas = []
+        root = xmltodict.parse(self.xml)
+        for cinema in root.get('cinemas', {}).get('cinema', []):
             cinema_data = {
-                'name': e.attrib.get('name', None),
-                'phone': e.attrib.get('phone', None),
-                'address': e.attrib.get('address', None),
-                'id': e.attrib.get('id', None),
-                'url': e.attrib.get('root', '') + e.attrib.get('url', '')
+                'name': cinema.get('@name', None),
+                'phone': cinema.get('@phone', None),
+                'address': self.build_address(cinema.get('@address', None), cinema.get('@postcode', None)),
+                'id': cinema.get('@id', None),
+                'url': cinema.get('@root', '') + cinema.get('@url', '')
             }
             if not self.check_cinema_exists(self.company_id, cinema_data['id']):
                 lat, long = self.get_coords(cinema_data['url'])
-                cinema = Cinema(
+                all_cinemas.append(Cinema(
                     cinema_name=cinema_data['name'],
                     company=self.company_id,
                     company_cinema_id=cinema_data['id'],
@@ -29,8 +30,15 @@ class CineworldCinemas(CinemaImport):
                     phone=cinema_data['phone'],
                     latitude=lat,
                     longitude=long
-                )
-                cinema.save()
+                ))
+        Cinema.objects.bulk_create(all_cinemas)
+        return {}
+
+    @staticmethod
+    def build_address(add, postcode):
+        if postcode:
+            return "{add}, {postcode}".format(**locals())
+        return add
 
     def get_coords(self, url):
         contents = self.get_url(url)
